67e0290f8c4f9ef076f5cb458cb94419a9e1f88c
Mironov Andrey <bgmir213@gmail.com>
2020-08-24 20:38:23 +0300

    Миграции пользовательской таблицы и направлений подготовки

---
+++ basic/migrations/m200817_071207_create_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m200817_071207_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('user', [
+            'id' => $this->primaryKey(),
+            'fName' => $this->string(),
+            'mName' => $this->string(),
+            'lName' => $this->string(),
+            'username' => $this->string(),
+            'password' => $this->string(),
+            'admin' => $this->integer().' NOT NULL DEFAULT 0',
+            'schedule' => $this->integer().' NOT NULL DEFAULT 0',
+            'inspector' => $this->integer().' NOT NULL DEFAULT 0',
+            'teacher' => $this->integer().' NOT NULL DEFAULT 0',
+            'groupManager' => $this->integer().' NOT NULL DEFAULT 0',
+            'applicantManager' => $this->integer().' NOT NULL DEFAULT 0',
+            'student' => $this->integer().' NOT NULL DEFAULT 1',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('user');
+    }
+}
