e7b81763b6c6e309fac22b3f0f74b56206b3f09c
Mironov Andrey <bgmir213@gmail.com>
2020-10-22 20:20:08 +0300

    Наугрузки у преподавателя

--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -57,6 +57,7 @@ AppAsset::register($this);
                 'label' => 'Преподаватель', 
                 'items' => [
                     ['label' => 'Журналы', 'url' => ['/teacher/journal']],
+                    ['label' => 'Нагрузки', 'url' => ['/teacher/teacherload']],
                 ]
                 ];
         }
