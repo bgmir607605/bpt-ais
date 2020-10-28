3d0a4bac9a1361b33ad7f497df3ebd3c73adfbb2
hatkidchan <hatkidchan@gmail.com>
2020-10-28 17:27:41 +0300

    Доделал календарь пропусков

--- basic/modules/student/views/skip/index.php
+++ basic/modules/student/views/skip/index.php
@@ -1,16 +1,49 @@
 <?php
 use app\widgets\CalendarWidget;
+use yii\helpers\Html;
+
+$skips_group = [];
+
+$cur_month = date('m'); $cur_year = date('Y');
+foreach($skips as $skip) {
+    list($year, $month, $day) = explode('-', $skip->schedule->date);
+    if($year == $cur_year && $month == $cur_month) {
+        if(!array_key_exists($day, $skips_group))
+            $skips_group[$day] = 0;
+        $skips_group[$day] += $skip->schedule->hours;
+    }
+}
+
 ?><div class="admin-default-index">
     <h1>Пропуски занятий</h1>
     <ul>
     <?php
-        foreach($skips as $skip){
-            echo '<p>'.$skip->schedule->date.' ('.$skip->schedule->number.' пара) '.$skip->schedule->hours.' ч.</p>';
-        }
+    $calend = CalendarWidget::begin();
+    foreach($skips_group as $day => $hours) {
+        $calend->addDay($day, sprintf('Часов пропущено: %s', $hours));
+    }
+    echo $calend->run();
     ?>
-    <?php $calend = CalendarWidget::begin(); ?>
-    <?php $calend->addDay(25, 'Test'); ?>
-    <?php $calend->addDay(28, 'Test'); ?>
-    <?= $calend->run(); ?>
+    <details>
+        <summary>Список пропусков</summary>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Дата</th>
+                    <th>Дисциплина</th>
+                    <th>№ пары</th>
+                    <th>Часов</th>
+                </tr>
+            </thead>
+            <tbody><?php foreach($skips as $skip) {
+                echo Html::beginTag('tr');
+                echo Html::tag('td', $skip->schedule->date);
+                echo Html::tag('td', $skip->schedule->teacherLoad->discipline->shortName);
+                echo Html::tag('td', $skip->schedule->number);
+                echo Html::tag('td', $skip->schedule->hours);
+                echo Html::endTag('tr');
+            } ?></tbody>
+        </table>
+    </details>
     </ul>
 </div>
