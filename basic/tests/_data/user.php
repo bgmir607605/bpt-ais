382ad973acd70b0698f88b10e3a61d2c5278eb4c
Mironov Andrey <bgmir213@gmail.com>
2020-10-28 07:21:30 +0300

    init tests

---
+++ basic/tests/_data/user.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'user1' => [
+        'username' => 'aa-mironov',
+        'password' => '$2y$10$wDxyfC6n19zA5yPk2ggZPOJcDH3oc4efapg6vEld.nfNdLowSZ2ve',
+        'lName' => 'Миронов',
+        'fName' => 'Андрей',
+        'mName' => 'Алексеевич',
+    ],
+//    'user2' => [
+//        'username' => 'napoleon69',
+//        'email' => 'aileen.barton@heaneyschumm.com',
+//        'auth_key' => 'dZlXsVnIDgIzFgX4EduAqkEPuphhOh9q',
+//        'password' => '$2y$13$kkgpvJ8lnjKo8RuoR30ay.RjDf15bMcHIF7Vz1zz/6viYG5xJExU6',
+//    ],
+];
--- basic/tests/unit/models/UserTest.php
+++ basic/tests/unit/models/UserTest.php
@@ -3,42 +3,55 @@
 namespace tests\unit\models;
 
 use app\models\User;
+use app\tests\fixtures\UserFixture;
 
 class UserTest extends \Codeception\Test\Unit
 {
+    
+    public function _fixtures()
+    {
+        return [
+            'profiles' => [
+                'class' => UserFixture::className(),
+                // fixture data located in tests/_data/user.php
+                'dataFile' => codecept_data_dir() . 'user.php'
+            ],
+        ];
+    }
+    
     public function testFindUserById()
     {
-        expect_that($user = User::findIdentity(100));
-        expect($user->username)->equals('admin');
+        expect_that($user = User::findIdentity(1));
+        expect($user->username)->equals('aa-mironov');
 
         expect_not(User::findIdentity(999));
     }
 
-    public function testFindUserByAccessToken()
-    {
-        expect_that($user = User::findIdentityByAccessToken('100-token'));
-        expect($user->username)->equals('admin');
-
-        expect_not(User::findIdentityByAccessToken('non-existing'));        
-    }
-
-    public function testFindUserByUsername()
-    {
-        expect_that($user = User::findByUsername('admin'));
-        expect_not(User::findByUsername('not-admin'));
-    }
-
-    /**
-     * @depends testFindUserByUsername
-     */
-    public function testValidateUser($user)
-    {
-        $user = User::findByUsername('admin');
-        expect_that($user->validateAuthKey('test100key'));
-        expect_not($user->validateAuthKey('test102key'));
-
-        expect_that($user->validatePassword('admin'));
-        expect_not($user->validatePassword('123456'));        
-    }
+//    public function testFindUserByAccessToken()
+//    {
+//        expect_that($user = User::findIdentityByAccessToken('100-token'));
+//        expect($user->username)->equals('admin');
+//
+//        expect_not(User::findIdentityByAccessToken('non-existing'));        
+//    }
+//
+//    public function testFindUserByUsername()
+//    {
+//        expect_that($user = User::findByUsername('admin'));
+//        expect_not(User::findByUsername('not-admin'));
+//    }
+//
+//    /**
+//     * @depends testFindUserByUsername
+//     */
+//    public function testValidateUser($user)
+//    {
+//        $user = User::findByUsername('admin');
+//        expect_that($user->validateAuthKey('test100key'));
+//        expect_not($user->validateAuthKey('test102key'));
+//
+//        expect_that($user->validatePassword('admin'));
+//        expect_not($user->validatePassword('123456'));        
+//    }
 
 }
