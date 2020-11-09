1a2763257a84b5db5071fc0fc1e8c06cb458b159
Mironov Andrey <bgmir213@gmail.com>
2020-11-09 14:03:21 +0300

    Абстракция над AR с фильтрами deleted

--- basic/models/NotDeletableAR.php
+++ basic/models/NotDeletableAR.php
@@ -1,8 +1,7 @@
 <?php
 namespace app\models;
 
-// needRefactoring Должен быть абстрактным?
-class NotDeletableAR extends \yii\db\ActiveRecord {
+abstract class NotDeletableAR extends \yii\db\ActiveRecord {
     /**
      * Очищенные от помеченных на удаление
      * @return type
@@ -25,20 +24,43 @@ class NotDeletableAR extends \yii\db\ActiveRecord {
         return self::find()->all();
     }
     
+    /**
+     * Устанавливает deleted = 1
+     * Фактически запись не удаляется
+     */
     public function delete() {
         $this->deleted = '1';
         $this->save();
         $this->deleteDependent();
     }
-    
     /**
      * Ппомечает удалёнными зависящие данные
      * Объявляется и рализуется в наследующих классах
      */
-//    protected function deleteDependent()
-//    {
-//       
-//    }
+    abstract protected function deleteDependent();
+    
+    /**
+     * Поиск только по записям, у которых deleted =1
+     */
+    public static function findDeleted(){
+        return parent::find()->onCondition(['and' ,['deleted' => '1']]);
+    }
+    /**
+     * Поиск только по записям по id, у которых deleted =1
+     */
+    public static function findOneDeleted($id = null){
+        return self::findDeleted()->where(['id' => $id])->one();
+    }
+    /**
+     * Все записи, у которых deleted = 1
+     * @return type
+     */
+    public static function findAllDeleted(){
+        return self::findDeleted()->all();
+    }
+    
+    // TODO confirmDelete()
+    
     
 }
 
