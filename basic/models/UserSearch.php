46757a9c781975e54111dab7960de4d5cf4f4b81
Mironov Andrey <bgmir213@gmail.com>
2020-12-18 07:12:56 +0300

    close #56

--- basic/models/UserSearch.php
+++ basic/models/UserSearch.php
@@ -5,6 +5,7 @@ namespace app\models;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use app\models\User;
+use yii\helpers\ArrayHelper;
 
 /**
  * UserSearch represents the model behind the search form of `app\models\User`.
@@ -55,8 +56,6 @@ class UserSearch extends User
             // $query->where('0=1');
             return $dataProvider;
         }
-        // Не показывать сущности, отмеченные удалёнными
-        $query->andFilterWhere(['deleted' => '0']);
         // grid filtering conditions
         $query->andFilterWhere([
             'id' => $this->id,
@@ -70,6 +69,40 @@ class UserSearch extends User
             'deleted' => $this->deleted,
         ]);
 
+        $query->andFilterWhere(['like', 'fName', $this->fName])
+            ->andFilterWhere(['like', 'mName', $this->mName])
+            ->andFilterWhere(['like', 'lName', $this->lName])
+            ->andFilterWhere(['like', 'username', $this->username])
+            ->andFilterWhere(['like', 'password', $this->password]);
+
+        return $dataProvider;
+    }
+    
+    public function searchForGroup($group, $params)
+    {
+        $query = User::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'sort'=>[
+                'defaultOrder'=>[
+                     'lName'=>SORT_ASC,
+                ]
+        ]]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+ 
+
+        $studentsIds = ArrayHelper::getColumn($group->students, 'id');
+        $query->andWhere(['in', 'id', $studentsIds]);
         $query->andFilterWhere(['like', 'fName', $this->fName])
             ->andFilterWhere(['like', 'mName', $this->mName])
             ->andFilterWhere(['like', 'lName', $this->lName])
